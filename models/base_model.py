698d92b01e4b1d49b6b4755cf9ee79fec3572e08
Jenna Jones <jljones121@gmail.com>
2024-07-02 13:21:18 -0500

    file_storage typos

--- models/base_model.py
+++ models/base_model.py
@@ -1,63 +1,10 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, DateTime
-import models
-from sqlalchemy.ext.declarative import declarative_base
-import MySQLdb
-from datetime import datetime
-import uuid
-import os
-<< << << < HEAD
-"""This module defines a base class for
-all models in hbnb clone"""
-
-
-class BaseModel:
-    """A base class for all hbnb models"""
-
-    def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        self.id = kwargs.get('id', str(uuid.uuid4()))
-        self.created_at = kwargs.get('created_at', datetime.now())
-        self.updated_at = kwargs.get('updated_at', datetime.now())
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = type(self).__name__
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
-
-    def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.now()
-        self.save_to_db()
-
-    def save_to_db(self):
-        """Saves instance data to MySQL database"""
-        db = MySQLdb.connect(host="localhost", user="your_username",
-                             passwd="your_password", db="your_db")
-        cursor = db.cursor()
-        query = """
-                INSERT INTO base_model (id, created_at, updated_at)
-                VALUES (%s, %s, %s)
-                """
-        cursor.execute(query, (self.id, self.created_at, self.updated_at))
-        db.commit()
-        db.close()
-
-    def to_dict(self):
-        """Convert instance into dict format"""
-        res = {}
-        for key, value in self.__dict__.items():
-            if key != '_sa_instance_state':
-                if isinstance(value, datetime):
-                    res[key] = value.isoformat()
-                else:
-                    res[key] = value
-        res['__class__'] = self.__class__.__name__
-        return res
-
-
-== == == =
 """BaseModel Module for HBNB project"""
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+import models
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
 
 
 Base = declarative_base()
@@ -134,6 +81,3 @@ class BaseModel:
         """ delete object
         """
         models.storage.delete(self)
-
-
->>>>>> > 630f12b63d127d3ae632c7d15c258eb4a6108daa
