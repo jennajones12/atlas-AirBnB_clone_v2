21e92bf5ba09b17f29852b4123f03243b2142505
Jenna Jones <jljones121@gmail.com>
2024-07-02 14:57:21 -0500

    test_console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,7 +37,7 @@ class FileStorage:
                 jo = json.load(f)
                 for key in jo:
                     self.__objects[key] = classes[jo[key]
-                                        ["__class__"]](**jo[key])
+                                         ["__class__"]](**jo[key])
         except FileNotFoundError:
             pass
 
