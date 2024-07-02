c5271eb6991050d9d691a84fbfd3e0f5eb3821b4
Jenna Jones <jljones121@gmail.com>
2024-07-02 13:46:43 -0500

    test_city

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,35 +1,204 @@
 #!/usr/bin/python3
-"""Test BaseModel Module"""
-import unittest
+"""Unit tests for BaseModel class and related functionality."""
 from models.base_model import BaseModel
+import unittest
 import datetime
+from uuid import UUID
+import json
+import os
+import pycodestyle
+
+
+class test_basemodel(unittest.TestCase):
+    """Tests for BaseModel class."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize test_basemodel instance."""
+        super().__init__(*args, **kwargs)
+        self.name = 'BaseModel'
+        self.value = BaseModel
+
+    def test_pycodestyle(self):
+        """Test that base_model.py conforms to PEP8."""
+        pycostyle = pycodestyle.StyleGuide(quiet=True)
+        result = pycostyle.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def setUp(self):
+        """Set up for tests."""
+        pass
+
+    def tearDown(self):
+        """Clean up after tests."""
+        try:
+            os.remove('file.json')
+        except Exception:
+            pass
+
+    def test_default(self):
+        """Test default instantiation."""
+        i = self.value()
+        self.assertEqual(type(i), self.value)
+
+    def test_kwargs(self):
+        """Test instantiation with kwargs."""
+        i = self.value()
+        copy = i.to_dict()
+        new = BaseModel(**copy)
+        self.assertFalse(new is i)
+
+    def test_kwargs_int(self):
+        """Test instantiation with invalid kwargs."""
+        i = self.value()
+        copy = i.to_dict()
+        copy.update({1: 2})
+        with self.assertRaises(TypeError):
+            new = BaseModel(**copy)
+
+    def test_save(self):
+        """Test save method."""
+        i = self.value()
+        i.save()
+        key = self.name + "." + i.id
+        with open('file.json', 'r') as f:
+            j = json.load(f)
+            self.assertEqual(j[key], i.to_dict())
+
+    def test_str(self):
+        """Test __str__ method."""
+        i = self.value()
+        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id, i.__dict__))
+
+    def test_todict(self):
+        """Test to_dict method."""
+        i = self.value()
+        n = i.to_dict()
+        self.assertEqual(i.to_dict(), n)
+
+    def test_kwargs_none(self):
+        """Test instantiation with None as kwargs."""
+        n = {None: None}
+        with self.assertRaises(TypeError):
+            new = self.value(**n)
+
+    def test_id(self):
+        """Test id attribute."""
+        new = self.value()
+        self.assertEqual(type(new.id), str)
+
+    def test_created_at(self):
+        """Test created_at attribute."""
+        new = self.value()
+        self.assertEqual(type(new.created_at), datetime.datetime)
+
+    def test_updated_at(self):
+        """Test updated_at attribute."""
+        new = self.value()
+        self.assertEqual(type(new.updated_at), datetime.datetime)
+        n = new.to_dict()
+        new = BaseModel(**n)
+        self.assertFalse(new.created_at == new.updated_at)
+
+    def test_uuid(self):
+        """Test UUID attribute."""
+        instance1 = BaseModel()
+        instance2 = BaseModel()
+        instance3 = BaseModel()
+        list_instances = [instance1, instance2, instance3]
+        for instance in list_instances:
+            ins_uuid = instance.id
+            with self.subTest(uuid=ins_uuid):
+                self.assertIs(type(ins_uuid), str)
+        self.assertNotEqual(instance1.id, instance2.id)
+        self.assertNotEqual(instance1.id, instance3.id)
+        self.assertNotEqual(instance2.id, instance3.id)
+
+    def test_str_method(self):
+        """Test __str__ method."""
+        instance6 = BaseModel()
+        string_output = "[BaseModel] ({}) {}".format(instance6.id, instance6.__dict__)
+        self.assertEqual(string_output, str(instance6))
+
+
+class TestCodeFormat(unittest.TestCase):
+    """Tests for PEP8 compliance."""
+
+    def test_pycodestyle(self):
+        """Test that base_model.py conforms to PEP8."""
+        pycostyle = pycodestyle.StyleGuide(quiet=True)
+        result = pycostyle.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
+class Test_docstrings(unittest.TestCase):
+    """Tests for docstrings."""
+
+    @classmethod
+    def setup_class(self):
+        """Set up for docstring tests."""
+        self.obj_members(BaseModel, inspect.isfunction)
 
 
 class TestBaseModel(unittest.TestCase):
-    """Test cases for the BaseModel class"""
+    """Tests for BaseModel class."""
 
-    def setUp(self):
-        """Setup method to initialize an instance before each test"""
-        self.model = BaseModel()
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the tests."""
+        cls.base = BaseModel()
+        cls.base.name = "Kev"
+        cls.base.num = 20
 
-    def test_id_is_assigned(self):
-        """Test that an id is assigned"""
-        self.assertIsNotNone(self.model.id)
+    @classmethod
+    def teardown(cls):
+        """Clean up after the tests."""
+        del cls.base
 
-    def test_created_at_is_assigned(self):
-        """Test that created_at is assigned"""
-        self.assertIsInstance(self.model.created_at, datetime.datetime)
+    def tearDown(self):
+        """Clean up after each test."""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
-    def test_updated_at_is_assigned(self):
-        """Test that updated_at is assigned"""
-        self.assertIsInstance(self.model.updated_at, datetime.datetime)
+    def test_pep8_BaseModel(self):
+        """Test that base_model.py conforms to PEP8."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        p = style.check_files(['models/base_model.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def test_save_updates_updated_at(self):
-        """Test that save updates the updated_at attribute"""
-        old_updated_at = self.model.updated_at
-        self.model.save()
-        self.assertNotEqual(self.model.updated_at, old_updated_at)
+    def test_checking_for_docstring_BaseModel(self):
+        """Check for docstrings in BaseModel."""
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+
+    def test_method_BaseModel(self):
+        """Check if BaseModel has the necessary methods."""
+        self.assertTrue(hasattr(BaseModel, "__init__"))
+        self.assertTrue(hasattr(BaseModel, "save"))
+        self.assertTrue(hasattr(BaseModel, "to_dict"))
+
+    def test_init_BaseModel(self):
+        """Test instantiation of BaseModel."""
+        self.assertTrue(isinstance(self.base, BaseModel))
+
+    def test_save_BaseModel(self):
+        """Test save method."""
+        self.base.save()
+        self.assertNotEqual(self.base.created_at, self.base.updated_at)
+
+    def test_to_dict_BaseModel(self):
+        """Test to_dict method."""
+        base_dict = self.base.to_dict()
+        self.assertEqual(self.base.__class__.__name__, 'BaseModel')
+        self.assertIsInstance(base_dict['created_at'], str)
+        self.assertIsInstance(base_dict['updated_at'], str)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,27 +1,82 @@
 #!/usr/bin/python3
-"""Test City Module"""
-import unittest
-from tests.test_models.test_base_model import TestBaseModel
+"""Unit tests for City class"""
+from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+import pycodestyle
 
 
-class TestCity(TestBaseModel):
-    """Test cases for the City class"""
+class test_City(test_basemodel):
+    """ """
 
-    def setUp(self):
-        """Setup method to initialize an instance before each test"""
-        self.model = City()
-        self.model.name = "San Francisco"
-        self.model.state_id = "CA"
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "City"
+        self.value = City
 
-    def test_name_is_assigned(self):
-        """Test that name is assigned"""
-        self.assertEqual(type(self.model.name), str)
+        def test_state_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.state_id), str)
 
-    def test_state_id_is_assigned(self):
-        """Test that state_id is assigned"""
-        self.assertEqual(type(self.model.state_id), str)
+    def test_name(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
 
 
-if __name__ == '__main__':
+class Test_PEP8(unittest.TestCase):
+    """test User"""
+
+    def test_pep8_user(self):
+        """test pep8 style"""
+         pep8style = pycodestyle.StyleGuide(quiet=True)
+         result = pep8style.check_files(['models/city.py'])
+         self.assertEqual(result.total_errors, 0,
+                  "Found code style errors (and warnings).")
+
+
+class TestCity(unittest.TestCase):
+    """Tests for the City class."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the tests."""
+        cls.city = City()
+        cls.city.name = "San Francisco"
+        cls.city.state_id = "CA"
+
+    @classmethod
+    def teardown(cls):
+        """Clean up after the tests."""
+        del cls.city
+
+    def tearDown(self):
+        """Clean up after each test."""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_pep8_City(self):
+        """Test that city.py conforms to PEP8."""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/city.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_checking_for_docstring_City(self):
+        """Check for docstrings in City."""
+        self.assertIsNotNone(City.__doc__)       
+
+    def test_save_City(self):
+        """Test save method."""
+        self.city.save()
+        self.assertNotEqual(self.city.created_at, self.city.updated_at)
+
+    def test_to_dict_City(self):
+        """Test to_dict method."""
+        self.assertEqual('to_dict' in dir(self.city), True)
+
+
+if __name__ == "__main__":
     unittest.main()
