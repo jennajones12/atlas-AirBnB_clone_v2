4a485a049f7e84485c7e1ff4f97ea8d135af25c5
Jenna Jones <jljones121@gmail.com>
2024-07-02 23:12:52 -0500

    test/city

--- models/city.py
+++ models/city.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column, String, ForeignKey
+import models
 from models.base_model import BaseModel
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
     """City class representing cities in a database."""
-    __tablename__ = 'cities'
-
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-
-    def __init__(self, *args, **kwargs):
-        """Initialize City object."""
-        super().__init__(*args, **kwargs)
-        self.state_id = kwargs.get('state_id', "")
-        self.name = kwargs.get('name', "")
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """Unit tests for City class"""
-import unittest
-import os
-import pycodestyle
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
@@ -18,68 +15,11 @@ class test_City(test_basemodel):
 
     def test_state_id(self):
         """Test state_id attribute type."""
-        new = self.value(state_id="CA")
+        new = self.value()
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
         """Test name attribute type."""
-        new = self.value(name="San Francisco")
+        new = self.value()
         self.assertEqual(type(new.name), str)
-
-
-class Test_PEP8(unittest.TestCase):
-    """test User"""
-
-    def test_pep8_user(self):
-        """test pep8 style"""
-        pep8style = pycodestyle.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/city.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-
-class TestCity(unittest.TestCase):
-    """Tests for the City class."""
-
-    @classmethod
-    def setUpClass(cls):
-        """Set up for the tests."""
-        cls.city = City()
-        cls.city.name = "San Francisco"
-        cls.city.state_id = "CA"
-
-    @classmethod
-    def tearDownClass(cls):
-        """Clean up after the tests."""
-        del cls.city
-
-    def tearDown(self):
-        """Clean up after each test."""
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
-    def test_pep8_City(self):
-        """Test that city.py conforms to PEP8."""
-        style = pycodestyle.StyleGuide(quiet=True)
-        p = style.check_files(['models/city.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
-    def test_checking_for_docstring_City(self):
-        """Check for docstrings in City."""
-        self.assertIsNotNone(City.__doc__)
-
-    def test_save_City(self):
-        """Test save method."""
-        self.city.save()
-        self.assertNotEqual(self.city.created_at, self.city.updated_at)
-
-    def test_to_dict_City(self):
-        """Test to_dict method."""
-        self.assertEqual('to_dict' in dir(self.city), True)
-
-
-if __name__ == "__main__":
-    unittest.main()
 
