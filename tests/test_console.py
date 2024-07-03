f7d1f4bc286751b15dfa51501864e996299b3b89
Jenna Jones <jljones121@gmail.com>
2024-07-02 20:49:59 -0500

    tst

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,8 +2,16 @@
 """Unit tests for HBNB console commands"""
 import unittest
 from unittest.mock import patch
-import pycodestyle
-import os
+from io import StringIO
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from console import HBNBCommand
+
 
 
 if __name__ == "__main__":
