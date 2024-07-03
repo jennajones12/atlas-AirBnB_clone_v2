c0618a6740868123353dbf9203350920b6d1d370
Jenna Jones <jljones121@gmail.com>
2024-07-02 23:05:32 -0500

    test/city

--- models/city.py
+++ models/city.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """City class for representing cities in a database."""
+    """City class representing cities in a database."""
     __tablename__ = 'cities'
 
-    name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Initialize City object."""
+        super().__init__(*args, **kwargs)
+        self.state_id = kwargs.get('state_id', "")
+        self.name = kwargs.get('name', "")
