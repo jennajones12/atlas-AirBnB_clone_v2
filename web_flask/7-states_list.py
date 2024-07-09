aa6f04fa7125526344e5d53a12e1fd8860ec60b7
Jenna Jones <jljones121@gmail.com>
2024-07-09 20:58:38 +0000

    7state

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Displays 'Hello HBNB!' """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Displays 'HBNB' """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Displays 'C' followed by the value of the text variable """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """ Displays 'Python' followed by the value of the text variable """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """ Displays 'n is a number' only if n is an integer """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ Displays a HTML page only if n is an integer """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ Displays a HTML page only if n is an integer """
+    return render_template('6-number_odd_or_even.html', n=n,
+                           type=("even" if n % 2 == 0 else "odd"))
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    from models import storage
+    from models.state import State
+
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+def get_name(item):
+    key, value = item
+    return vlaue.name
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    from models import storage
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
