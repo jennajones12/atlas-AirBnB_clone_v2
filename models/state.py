af81a1538e88d35dac0b0d3773f919be197a4596
Jenna Jones <jljones121@gmail.com>
2024-07-09 21:02:12 +0000

    state.py update

--- models/state.py
+++ models/state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """State Module for HBNB project"""
-from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base, storage_type
+from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from models.city import City
 
 
 class State(BaseModel, Base):
     """State class representing states in a database."""
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
 
-    cities = relationship(
-        'City', cascade="all, delete", backref='state'
-    )
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
     @property
     def cities(self):
