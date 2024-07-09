44509cba2118ee82dde43fd18f27453ddd4cdeec
Jenna Jones <jljones121@gmail.com>
2024-07-09 20:51:47 +0000

    pep8

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,6 +42,7 @@ def number_template(n):
     """ Displays a HTML page only if n is an integer """
     return render_template('5-number.html', n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """ Displays a HTML page only if n is an integer """
