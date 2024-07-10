da7e8d627ee5a13eddffe7127b7c3c76da504cdf
Jenna Jones <jljones121@gmail.com>
2024-07-09 22:40:19 -0500

    task 7

--- models/city.py
+++ models/city.py
@@ -3,13 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
-
 class City(BaseModel, Base):
     """City class representing cities in a database."""
     __tablename__ = 'cities'
     id = Column(String(60), primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(
-        String(60),
-        ForeignKey('states.id'),
-        nullable=False)  # Ensure state_id is VARCHAR(60)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
