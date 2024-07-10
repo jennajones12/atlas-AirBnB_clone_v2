5efefb77ee585ab4b34ccaa1c9ad75ef1ce80cd1
Jenna Jones <jljones121@gmail.com>
2024-07-10 18:51:23 -0500

    task8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """ Remove the current SQLAlchemy Session """
     storage.close()
+
 
 @app.route('/states_list', methods=['GET'])
 def states_list():
@@ -17,6 +19,7 @@ def states_list():
     sorted_states = sorted(states, key=lambda state: state.name)
 
     return render_template('7-states_list.html', states=sorted_states)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
