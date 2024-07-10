568cd03aa40cf488aa4fbfe1c3191acea88af377
Jenna Jones <jljones121@gmail.com>
2024-07-10 15:00:23 -0500

    task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -90,7 +90,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(session_factory)()
+        self.__session = scoped_session(session_factory)
 
     def link_amenity(self, amenity_id, place_id):
         """ Add an amenity to a place """
